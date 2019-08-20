8a2332265eee4924fb1287143115de7e792eacbf
Muthukumar Thangavinayagam <2063172+mkpracticalworks@users.noreply.github.com>
2019-08-20 20:48:27 +0530

    Remove OneToOne project

--- dn-jpa-one-to-one-example/src/main/java/com/dn/jpa/relationship/example/repository/EmployeeProfileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dn.jpa.relationship.example.repository;
-
-import com.dn.jpa.relationship.example.model.EmployeeProfile;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeProfileRepository extends JpaRepository<EmployeeProfile,Long> {
-}
