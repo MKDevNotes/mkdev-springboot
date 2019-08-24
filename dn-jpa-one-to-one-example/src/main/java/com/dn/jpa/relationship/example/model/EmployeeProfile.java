8b7f3ba4e1ce6a292490b1263710f475c09553b4
Muthukumar Thangavinayagam <2063172+mkpracticalworks@users.noreply.github.com>
2019-08-24 18:44:04 +0530

    Spring Boot JPA One To One Mapping Example

---
+++ dn-jpa-one-to-one-example/src/main/java/com/dn/jpa/relationship/example/model/EmployeeProfile.java
@@ -0,0 +1,137 @@
+package com.dn.jpa.relationship.example.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.Date;
+
+/***
+ * @author Muthukumar Thangavinayagam
+ */
+
+@Entity
+@Table(name = "employee_profiles")
+public class EmployeeProfile implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "phone_number")
+    @Size(max = 15)
+    private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 10)
+    private Gender gender;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "dob")
+    private Date dateOfBirth;
+
+    @Size(max = 100)
+    private String address;
+
+    @Size(max = 50)
+    private String city;
+
+    @Size(max = 50)
+    private String state;
+
+    @Size(max = 50)
+    private String country;
+
+    @Column(name = "zip_code")
+    @Size(max = 32)
+    private String zipCode;
+
+    @OneToOne(fetch = FetchType.LAZY,optional = false)
+    @JoinColumn(name = "employee_id",nullable = false)
+    private Employee employee;
+
+    public EmployeeProfile() {
+    }
+
+    public EmployeeProfile(String phoneNumber, Gender gender, Date dateOfBirth, String address, String city, String state, String country, String zipCode) {
+        this.phoneNumber = phoneNumber;
+        this.gender = gender;
+        this.dateOfBirth = dateOfBirth;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.country = country;
+        this.zipCode = zipCode;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+}
