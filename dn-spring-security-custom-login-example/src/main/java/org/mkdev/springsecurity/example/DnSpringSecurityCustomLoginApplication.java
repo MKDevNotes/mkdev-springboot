55e72f204ed49535d371059cecbbf4f025ddacd8
muthukumar <muthukumar.wit@gmail.com>
2019-09-25 15:36:31 +0530

    Resolved /error stats 500 issue

--- dn-spring-security-custom-login-example/src/main/java/org/mkdev/springsecurity/example/DnSpringSecurityCustomLoginApplication.java
+++ dn-spring-security-custom-login-example/src/main/java/org/mkdev/springsecurity/example/DnSpringSecurityCustomLoginApplication.java
@@ -1,9 +1,12 @@
 package org.mkdev.springsecurity.example;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 public class DnSpringSecurityCustomLoginApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +14,7 @@ public class DnSpringSecurityCustomLoginApplication {
 	}
 
 }
+
+
+
+// To Resolve /error status:500 issue.  @EnableAutoConfiguration -> ErrorMvcAutoConfiguration.css - > 
