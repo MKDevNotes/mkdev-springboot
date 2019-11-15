a831ad23f7b4fe22a17c30b33daea985125fe590
muthukumar <muthukumar.wit@gmail.com>
2019-11-15 15:58:29 +0530

    Updated url logic

--- dn-springboot-security-example/src/main/java/org/mkdev/springboot/security/cotroller/DnSpringSecurityController.java
+++ dn-springboot-security-example/src/main/java/org/mkdev/springboot/security/cotroller/DnSpringSecurityController.java
@@ -1,5 +1,7 @@
 package org.mkdev.springboot.security.cotroller;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,10 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DnSpringSecurityController {
 
-	@RequestMapping("/rest/sayHello")
-	public String sayHello() {
-
-		return "Hello receiver";
+	@RequestMapping({ "/", "/login" })
+	public String welcome(Map<String, Object> model) {
+		return "login";
 	}
 
 }
