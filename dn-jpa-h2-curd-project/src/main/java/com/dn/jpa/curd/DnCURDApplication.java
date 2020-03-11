f9c96afc036cf2854908803346c6b7895c6b8070
Muthukumar Thangavinayagam <muthukumar.wit@gmail.com>
2020-03-11 10:09:58 +0000

    CURD Sample Application with H2

---
+++ dn-jpa-h2-curd-project/src/main/java/com/dn/jpa/curd/DnCURDApplication.java
@@ -0,0 +1,13 @@
+package com.dn.jpa.curd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnCURDApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnCURDApplication.class, args);
+	}
+
+}
