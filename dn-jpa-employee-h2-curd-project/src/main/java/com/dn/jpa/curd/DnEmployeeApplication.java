e2bcac01209751ff4b48f15eeac254fbdf71fea2
Muthukumar Thangavinayagam <muthukumar.wit@gmail.com>
2020-03-12 07:39:11 +0000

    Employee CURD inital code commit

---
+++ dn-jpa-employee-h2-curd-project/src/main/java/com/dn/jpa/curd/DnEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.dn.jpa.curd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnEmployeeApplication.class, args);
+	}
+
+}
